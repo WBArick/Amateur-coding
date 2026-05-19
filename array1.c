bfd5f332b015ee4defb448fd354e4baae77f9f13
WBArick <wahibarick@gmail.com>
2026-05-19 14:35:41 +0600

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a[3]={67,76,96};
+    printf("First element:%d\n",a[0]);
+    printf("Second element:%d\n",a[1]);
+    printf("Third element:%d\n",a[2]);
+    printf("Size of array a=%d\n",sizeof(a));
+    return 0;
+}
