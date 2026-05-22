71654af4cd3733a2c10208615ddf74c047419d23
WBArick <wahibarick@gmail.com>
2026-05-23 00:31:42 +0600

    Add files via upload

---
+++ malloc1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *p;
+    p=malloc(sizeof(int)*3);
+    *p=11;
+    *(p+1)=22;
+    *(p+2)=33;
+    printf("%d %d %d\n",*p,*(p+1),*(p+2));
+    return 0;
+
+}
