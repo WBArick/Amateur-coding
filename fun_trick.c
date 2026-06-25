49e970062262e41100900b856e1bb929bf389597
WBArick <wahibarick@gmail.com>
2026-06-26 02:12:21 +0600

    Add files via upload

---
+++ fun_trick.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Feeling hopeless?\r");
+    getch();
+    printf("Don't give up. keep pushing. You can do it man!!");
+    return 0;
+}
