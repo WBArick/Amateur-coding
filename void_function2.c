2441726d51cb4b69521ef9208f2bb6c206d5a8ea
WBArick <wahibarick@gmail.com>
2026-06-29 21:51:57 +0600

    Add files via upload

---
+++ void_function2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void circle();
+int main()
+{
+    printf("Here are two circles:\n");
+    circle();
+    circle();
+}
+void circle()
+{
+    printf("     **     \n");
+    printf("***      *** \n");
+    printf("***      *** \n");
+    printf("     **      \n");
+}
